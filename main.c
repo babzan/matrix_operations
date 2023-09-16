c4e5bb90772f86616c2e1caaa4a1de7d3231bc68
Babak Huseynov <mirza.bhuseynov@gmail.com>
2023-09-16 21:55:03 +0300

    added rotation

--- main.c
+++ main.c
@@ -13,13 +13,14 @@ int main(int argc, char* argv[]){
         if (operation == -1) {
             continue;
         } else {
-            if (operation == 4 || operation == 5) {
-                if (scanf("%d%d%c", &rows1, &cols1, &symbol) == 3 && (symbol == ' ' || symbol == '\n')) {
+            // if (operation == 4 || operation == 5) {
+            //     if (scanf("%d%d%c", &rows1, &cols1, &symbol) == 3 && (symbol == ' ' || symbol == '\n')) {
 
-                } else {
-                    printf("Please, write correct number of ROWS and COLUMNS. For example: 2 2");
-                }
-            }
+            //     } else {
+            //         printf("Please, write correct number of ROWS and COLUMNS. For example: 2 2");
+            //     }
+            // }
         }
     }
+    return 0;
 }
--- operations/transform.c
+++ operations/transform.c
@@ -1,5 +1,42 @@
 #include "../headers/matrix.h"
 
+void rotateClockwise90(int **matrix, int rows, int cols);
+void rotateCounterClockwise90(int **matrix, int rows, int cols);
+void rotateClockwise180(int **matrix, int rows, int cols);
+void rotateCounterClockwise180(int **matrix, int rows, int cols);
+void printResult(int **matrix, int rows, int cols)
+
 void transform(int **matrix, int rows, int cols) {
+    int **result;
+    memoryAllocate(result, rows, cols);
+    result = transpose(matrix, rows, cols);
+    rotateClockwise90(result, cols, rows);
+}
+
+void rotateClockwise90(int **matrix, int rows, int cols) {
+    printf("Rotated matrix. 90 degree clockwise.");
+    for (int i = 0; i < rows; i++) {
+        for (int j = cols - 1; j > -1; j--) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void rotateCounterClockwise90(int **matrix, int rows, int cols) {
+    printf("Rotated matrix. 90 degree counter clockwise.");
+    for (int i = rows - 1; i > -1; i--) {
+        for (int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void rotateClockwise180(int **matrix, int rows, int cols) {
+
+}
+
+void rotateCounterClockwise180(int **matrix, int rows, int cols) {
 
 }
